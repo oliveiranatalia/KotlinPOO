b5ffbf31def6c93c15b1855ddb90215466b269f8
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-05-08 18:25:10 -0300

    Update Losango.kt

--- aula23/geometria/Losango.kt
+++ aula23/geometria/Losango.kt
@@ -1,8 +1,8 @@
 package poo.aula23.geometria
 
-class Losango(override var b: Double, override var h: Double) :FiguraGeometrica{
-    fun receberMedidas(){
+class Losango(override var b: Double, override var h: Double) : FiguraGeometrica {
+    override fun calcularMedidas(){
         val resultado =  b * h / 2
-        println("A area do losango é $resultado.")
+        println("A area do losango Ã© $resultado.")
     }
 }
