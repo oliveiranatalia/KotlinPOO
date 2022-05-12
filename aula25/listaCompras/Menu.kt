8ca72694c6cfb81fc056d7e5e804d873fa30844c
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-05-12 14:28:27 -0300

    corrigir menu

--- aula25/listaCompras/Menu.kt
+++ aula25/listaCompras/Menu.kt
@@ -3,25 +3,25 @@ package poo.aula25.listaCompras
 import kotlin.system.exitProcess
 
 class Menu {
-    fun menuPrincipal(carrinho:Carrinho){
+    fun menuPrincipal(){
         try{
-            print("\nInsira a referÍncia de como deseja prosseguir:\n\n" +
+            print("\nInsira a refer√™ncia de como deseja prosseguir:\n\n" +
                     "--Inserir produto no carrinho (i)\n" +
                     "----------Visualizar carrinho (v)\n" +
                     "---------------------Encerrar (e)\n ->  ")
             when (readln()) {
-                "i" -> {val i = carrinho.inserirCarrinho()
-                    menuPrincipal(carrinho)}
-                "v" -> {val v = carrinho.exibirCarrinho()
-                    menuPrincipal(carrinho)}
-                "e" -> {println("\nFechando, atÈ mais ....")
+                "i" -> {val i = Carrinho().inserirCarrinho()
+                    menuPrincipal()}
+                "v" -> {val v = Carrinho().exibirCarrinho()
+                    menuPrincipal()}
+                "e" -> {println("\nFechando, at√© mais ....")
                     exitProcess(0)}
-                else -> {throw IllegalArgumentException ("Valor inv·lido")
+                else -> {throw IllegalArgumentException ("Valor inv√°lido")
                 }
             }
         }catch(e:Exception){
             print(e.message)
-            menuPrincipal(carrinho)
+            menuPrincipal()
         }
     }
 }
